1faf4f50dccecb987f41aeacf475c37fdf1d0097
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2021-09-04 15:29:57 +0300

    feat: Replace transaction in repo.AddServices with bulk-insert

--- internal/repo/pg_repo.go
+++ internal/repo/pg_repo.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-
 	"github.com/google/uuid"
 	"github.com/ozonva/ova-service-api/internal/models"
 	"github.com/rs/zerolog/log"
+	"strings"
 
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
@@ -44,31 +44,22 @@ func (repo *PostgresServiceRepo) AddServices(services []models.Service) error {
 		return nil
 	}
 
-	tx, err := repo.db.BeginTx(repo.ctx, nil)
-	defer func(tx *sql.Tx) {
-		txErr := tx.Rollback()
-		if txErr != sql.ErrTxDone {
-			log.Err(txErr).Msg("Can' rollback the transaction")
-		}
-	}(tx)
+	queryParts := make([]string, len(services)+1)
+	queryParts[0] = "INSERT INTO services (id, user_id, description, service_name, service_address, when_local, when_utc) VALUES "
 
-	if err != nil {
+	queryValues := make([]interface{}, 0)
+
+	for i, service := range services {
+		// This looks crazy, but this is because of placeholders in pgx: $1, $2, $3, etc. I can't find a way how to do it easier using database/sql.
+		queryParts[i+1] = fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d),", 7*i+1, 7*i+2, 7*i+3, 7*i+4, 7*i+5, 7*i+6, 7*i+7)
+		queryValues = append(queryValues, service.ID, service.UserID, service.Description, service.ServiceName, service.ServiceAddress, service.WhenLocal, service.WhenUTC)
+	}
+
+	query := strings.Join(queryParts, "")
+	query = query[:len(query)-1] // Remove trailing comma
+
+	if _, err := repo.db.ExecContext(repo.ctx, query, queryValues...); err != nil {
 		log.Err(err).Msg("Failed to begin transaction")
-		return err
-	}
-
-	query := `INSERT INTO services (id, user_id, description, service_name, service_address, when_local, when_utc)
-			VALUES ($1, $2, $3, $4, $5, $6, $7)`
-
-	for _, service := range services {
-		if _, err = tx.ExecContext(repo.ctx, query, service.ID, service.UserID, service.Description, service.ServiceName, service.ServiceAddress, service.WhenLocal, service.WhenUTC); err != nil {
-			log.Err(err).Msg("Failed to begin transaction")
-			return err
-		}
-	}
-
-	if err = tx.Commit(); err != nil {
-		log.Err(err).Msg("Error occurred during commit transaction")
 		return err
 	}
 
